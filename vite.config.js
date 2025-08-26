6c8655919efab5e0be933c22c0c42643a960baf3
agcproplus-svg <agcproplus@gmail.com>
2025-08-25 21:47:00 -0400

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// Update base path for GitHub Pages
 export default defineConfig({
   plugins: [react()],
-  base: './'
+  base: '/in-the-trenches/'
 })
